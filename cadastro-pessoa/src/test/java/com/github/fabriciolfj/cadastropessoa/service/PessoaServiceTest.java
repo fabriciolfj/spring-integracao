dae9c7b603c68b50b18e228167dc441ae01cd51b
fabricio <fabricio.jacob@outlook.com>
2020-07-26 23:22:11 -0300

    add

--- cadastro-pessoa/src/test/java/com/github/fabriciolfj/cadastropessoa/service/PessoaServiceTest.java
+++ cadastro-pessoa/src/test/java/com/github/fabriciolfj/cadastropessoa/service/PessoaServiceTest.java
@@ -6,13 +6,22 @@ import com.github.fabriciolfj.cadastropessoa.api.dto.request.PessoaResquestDTO;
 import com.github.fabriciolfj.cadastropessoa.api.dto.response.PessoaResponseDTO;
 import com.github.fabriciolfj.cadastropessoa.domain.service.PessoaService;
 import com.github.fabriciolfj.cadastropessoa.wiremock.MockLogradouro;
+
+import io.restassured.RestAssured;
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.response.Response;
 import lombok.RequiredArgsConstructor;
 import org.json.JSONException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import wiremock.org.apache.http.HttpStatus;
+import wiremock.org.hamcrest.CoreMatchers;
 
 import static com.github.fabriciolfj.cadastropessoa.template.loader.PessoaRequestDTOLoader.PESSOA_REQUEST_VALIDA;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -20,10 +29,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class PessoaServiceTest extends CadastroPessoaApplicationTests {
 
     private final PessoaService pessoaService;
+    private static final String URL = "/pessoas";
 
     @Test
     @DisplayName("Criando uma pessoa")
-    public void testWhenPeronCreated() throws JSONException {
+    public void testWhenPersonCreated() throws JSONException {
         final PessoaResquestDTO request = Fixture.from(PessoaResquestDTO.class)
                 .gimme(PESSOA_REQUEST_VALIDA);
 
@@ -32,5 +42,25 @@ public class PessoaServiceTest extends CadastroPessoaApplicationTests {
 
         assertTrue(response.getLogradouros().size() > 0);
         assertTrue(response.getNome() != null);
+    }
+
+    @Test
+    @DisplayName("Teste controller")
+    public void testWenApi() throws JSONException {
+        final PessoaResquestDTO request = Fixture.from(PessoaResquestDTO.class)
+                .gimme(PESSOA_REQUEST_VALIDA);
+
+        MockLogradouro.getLogradouro("14150-000");
+
+        RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .post(URL)
+                .then()
+                //Assert
+                .statusCode(HttpStatus.SC_CREATED)
+                .body(notNullValue())
+                .body("nome", is("teste teste"))
+                .body("logradouros", notNullValue());
     }
 }
--- cadastro-pessoa/src/test/java/com/github/fabriciolfj/cadastropessoa/template/loader/PessoaRequestDTOLoader.java
+++ cadastro-pessoa/src/test/java/com/github/fabriciolfj/cadastropessoa/template/loader/PessoaRequestDTOLoader.java
@@ -8,12 +8,20 @@ import com.github.fabriciolfj.cadastropessoa.api.dto.request.PessoaResquestDTO;
 public class PessoaRequestDTOLoader implements TemplateLoader {
 
     public static final String PESSOA_REQUEST_VALIDA = "requisicaoValida";
+    public static final String PESSOA_REQUEST_INVALIDA = "requisicaoInvalida";
     @Override
     public void load() {
         Fixture.of(PessoaResquestDTO.class).addTemplate(PESSOA_REQUEST_VALIDA, new Rule() {
             {
                 add("cep", "14150-000");
                 add("nome", "teste teste");
+            }
+        });
+
+        Fixture.of(PessoaResquestDTO.class).addTemplate(PESSOA_REQUEST_INVALIDA, new Rule() {
+            {
+                add("cep", null);
+                add("nome", null);
             }
         });
 
